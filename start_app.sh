f9d710188f9a765b5cb477c30d7d1bc9be392502
Phani Adabala <38834236+adabalap@users.noreply.github.com>
2025-05-23 15:23:59 +0530

    Update start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -1,3 +1,3 @@
-export GEMINI_API_KEY="AIzaSyAnp0lHEZnajMbDavNPrrTUhSGareRwFp0"
+export GEMINI_API_KEY=""
 gunicorn --bind 0.0.0.0:5000 --certfile cert.pem --keyfile key.pem app:app >> app.log 2>&1 &
 #gunicorn --bind 0.0.0.0:5000 app:app 
